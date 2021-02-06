8a270d84550422e983b844859c22d5594ce380c2
Princess Onyiri <princessonyiri@gmail.com>
2021-02-06 13:44:18 -0500

    final version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-// const select= document.getElementsByClassName('selector');
+
 const chosen= document.getElementsByClassName('entire-box')[0];
 
 function changecol(event){
@@ -9,8 +9,8 @@ function changecol(event){
     
     if (targ.className=== 'selector'){
         targ.classList.toggle('white');
-        console.log(targ.className);
-    } else if (targ.className=== 'selector white'){
+    }
+     else if (targ.className=== 'selector white'){
         targ.className= 'selector';
     }
     
@@ -20,16 +20,13 @@ chosen.addEventListener('click', changecol)
 
 function moveitems(event){
     const targ= event.target;
-    const list= document.getElementsByClassName('box');
     const childs= targ.parentElement;
-    // console.log(childs.querySelectorAll('.selector'))
     
     if (targ.className==='move-left move' && childs.querySelector('.selector')){
-        // console.log(childs.querySelector('.selector'))
-        // console.log(childs);
+        
         [...childs.querySelectorAll('.selector')].filter(elem=> elem.className!== 'selector white').forEach(elem=> childs.previousElementSibling.appendChild(elem));
         
-        // childs.previousElementSibling.append(childs.querySelectorAll('.selector'));
+        
         
     } else if (targ.className==='move-right move' && childs.querySelector('.selector')){
         [...childs.querySelectorAll('.selector')].filter(elem=> elem.className!== 'selector white').forEach(elem=> childs.nextElementSibling.appendChild(elem));
