c5108753c3d4a965e316a2333234ad4a16774c63
Zaina Rodney <zainarodney@gmail.com>
2021-02-11 22:49:40 -0500

    added feedback

---
+++ feedback.md
@@ -0,0 +1,3 @@
+Great job, Princess! This works perfectly and I like that your code is very readable and simple!
+I made a couple comments in the script.js file but they were really minor! Some of them were around code hygiene.
+One thing you might want to consider is using a formatter like prettier or anything else you prefer, so that it breaks your code up and makes it more readable.
--- script.js
+++ script.js
@@ -1,38 +1,50 @@
+const chosen = document.getElementsByClassName('entire-box')[0];
 
-
-
-const chosen= document.getElementsByClassName('entire-box')[0];
-
+/*remember that javascript uses camelCase as a naming convention
+so this should be changeCol */
 function changecol(event){
-    
+
     const targ= event.target;
-    
-    if (targ.className=== 'selector'){
+    /* you're repeating some of the logic that is included in the toggle function
+    if the element doesnt have the class name, it will add it, if the element does have the class name,
+    it removes it.
+    if you want to be sure you're targeting the right class you could also use targ.matches('.selector')
+    which works similary to querySelector in that it takes any valid CSS, but will return true or false if the
+    element matches the css. so the code you wrote below actually works the same as
+        if (targ.matches('.selector')) targ.classList.toggle('white')
+    */
+    if (targ.className === 'selector') {
         targ.classList.toggle('white');
     }
      else if (targ.className=== 'selector white'){
         targ.className= 'selector';
     }
-    
-   
+
+
 }
 chosen.addEventListener('click', changecol)
 
+//moveItems
 function moveitems(event){
-    const targ= event.target;
+    const targ = event.target;
+    /*
+    you want your code to be "self-documented" so that when someone else is working on it,
+    it's clear what the variable names are.
+    renaming 'childs' to something like 'parent' makes more sense.
+    */
     const childs= targ.parentElement;
-    
+
     if (targ.className==='move-left move' && childs.querySelector('.selector')){
-        
-        [...childs.querySelectorAll('.selector')].filter(elem=> elem.className!== 'selector white').forEach(elem=> childs.previousElementSibling.appendChild(elem));
-        
-        
-        
+        [...childs.querySelectorAll('.selector')]
+            .filter(elem => elem.className !== 'selector white')
+            .forEach(elem => childs.previousElementSibling.appendChild(elem));
     } else if (targ.className==='move-right move' && childs.querySelector('.selector')){
-        [...childs.querySelectorAll('.selector')].filter(elem=> elem.className!== 'selector white').forEach(elem=> childs.nextElementSibling.appendChild(elem));
+        [...childs.querySelectorAll('.selector')]
+            .filter(elem => elem.className !== 'selector white')
+            .forEach(elem => childs.nextElementSibling.appendChild(elem));
     }
-   
-}  
+
+}
 chosen.addEventListener('click', moveitems);
 
 
